af847a9b67e5409611f0803881c04100c4c2f87d
shakib <shakibabdullah500@gmail.com>
2026-09-24 21:52:16 +0600

    cmnt cp next added

---
+++ Linked List/Linked list-2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+
+};
+
+
+void insert_at_head(Node* &head ,int val){
+    Node* newnode = new Node(val);
+    newnode->next = head;
+    head = newnode;
+
+}
+
+void print_Linked_list(Node* head){
+    Node* temp = head;
+    while( temp!= NULL){
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main(){
+
+    Node* head =  new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+//    b->next = NULL;
+
+    cout << head->val << endl;
+    cout << a->val << endl;       // value of a
+    cout << b->val << endl;
+
+    insert_at_head(head , 5);       // value of b
+
+    print_Linked_list(head);
+
+    return 0;
+}
--- Linked List/linklist-1.cpp
+++ Linked List/linklist-1.cpp
@@ -27,11 +27,17 @@ int main(){
     b.next = &c;
     c.next = NULL;
 
-    cout << a.val << endl;       // value of a
-    cout << b.val << endl;        // value of b
-    cout << (*a.next).val << endl;  // value of b
-    cout << a.next->val << endl;      // value of b
-    cout << a.next->next->val << endl;  // value of C
+//    cout << a.val << endl;       // value of a
+//    cout << b.val << endl;        // value of b
+//    cout << (*a.next).val << endl;  // value of b
+//    cout << a.next->val << endl;      // value of b
+//    cout << a.next->next->val << endl;  // value of C
+
+    Node* temp = &a;
+    while( temp != NULL){
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
 
 
     return 0;
